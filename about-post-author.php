0e25da7ea463ea76331df929d24d10d60d1a4b88
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-01-17 11:27:12 +0600

    update escape string

--- about-post-author.php
+++ about-post-author.php
@@ -107,24 +107,24 @@ function about_post_author_settings_page() {
 	?>
 	<div class="about-post-author-main">
 		<div class="about-post-author-body about-post-author-common">
-			<h1 id="page-title"><?php print esc_attr( 'About Post Author Settings' ); ?></h1>
+			<h1 id="page-title"><?php print esc_html__( 'About Post Author Settings', 'about-post-author' ); ?></h1>
 			<form action="options.php" method="post">
 				<?php wp_nonce_field( 'update-options' ); ?>
 				<!-- Background Color -->
-				<label for="about-post-author-background-color" name="about-post-author-background-color"><?php print esc_html__( 'Background Color', 'about-post-author' ); ?></label>
+				<label for="about-post-author-background-color" name="about-post-author-background-color"><?php echo esc_html__( 'Background Color', 'about-post-author' ); ?></label>
 				<input type="color" id="about-post-author-background-color" name="about-post-author-background-color" value="<?php echo esc_attr( get_option('about-post-author-background-color') ); ?>">
 				<!-- Text Color -->
-				<label for="about-post-author-text-color" name="about-post-author-text-color"><?php print esc_html__( 'Text Color', 'about-post-author' ); ?></label>
+				<label for="about-post-author-text-color" name="about-post-author-text-color"><?php echo esc_html__( 'Text Color', 'about-post-author' ); ?></label>
 				<input type="color" id="about-post-author-text-color" name="about-post-author-text-color" value="<?php echo esc_attr( get_option('about-post-author-text-color') ); ?>">
 
 				<!-- input type -->
 				<input type="hidden" name="action" value="update">
 				<input type="hidden" name="page_options" value="about-post-author-background-color, about-post-author-text-color">
-				<input class="button button-primary" type="submit" name="submit" value="<?php esc_attr_e('Save Changes', 'about-post-author'); ?>">
+				<input class="button button-primary" type="submit" name="submit" value="<?php esc_attr_e( 'Save Changes', 'about-post-author' ); ?>">
 			</form>
 		</div>
 		<div class="about-post-author-aside about-post-author-common">
-			<h2 class="aside-title"><?php print esc_attr( 'Plugin Author Info' ); ?></h2>
+			<h2 class="aside-title"><?php echo esc_html__( 'Plugin Author Info', 'about-post-author' ); ?></h2>
 			<div class="author-card">
 				<a class="link" href="https://profiles.wordpress.org/mehrazmorshed/" target="_blank">
 					<img class="center" src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . 'assets/images/author.png' ); ?>" width="128px">
@@ -138,7 +138,7 @@ function about_post_author_settings_page() {
 					<a class="link" href="https://www.youtube.com/@mehrazmorshed" target="_blank"><span class="dashicons dashicons-youtube"></span></a>
 				</h1>
 			</div>
-			<h3 class="aside-title"><?php print esc_attr( 'Other Useful Plugins' ); ?></h3>
+			<h3 class="aside-title"><?php echo esc_attr_e( 'Other Useful Plugins', 'about-post-author' ); ?></h3>
 			<div class="author-card">
 				<a class="link" href="<?php echo esc_url('https://wordpress.org/plugins/hide-titles'); ?>" target="_blank">
 					<span class="dashicons dashicons-admin-plugins"></span> <b><?php echo esc_html__( 'Hide Titles', 'about-post-author' ); ?></b>
@@ -156,9 +156,9 @@ function about_post_author_settings_page() {
 					<span class="dashicons dashicons-admin-plugins"></span> <b><?php echo esc_html__( 'Hide Thumbnails', 'about-post-author' ); ?></b>
 				</a>
 			</div>
-			<h3 class="aside-title"><?php print esc_attr( 'About Author Meta' ); ?></h3>
+			<h3 class="aside-title"><?php echo esc_attr_e( 'About Author Meta', 'about-post-author' ); ?></h3>
 			<a class="link" href="https://www.buymeacoffee.com/mehrazmorshed" target="_blank">
-				<button class="button button-primary btn"><?php print esc_attr( 'Donate To This Plugin' ); ?></button>
+				<button class="button button-primary btn"><?php echo esc_attr_e( 'Donate To This Plugin', 'about-post-author' ); ?></button>
 			</a>
 		</div>
 	</div>
